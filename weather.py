b0046ad6e8201f30c06da1226f61c4ef352fa8d6
sivanwu <1984430988@qq.com>
2019-01-04 17:50:40 +0800

    add input to file

--- weather.py
+++ weather.py
@@ -1,11 +1,19 @@
 # -*- coding:UTF-8 -*-
 
 import requests
+import json
 from twilio.rest import Client
 import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+def saveToFile(message):
+    with open("test.txt", "a") as f:
+        message = message + "\n"
+        f.write(message.encode("gbk"))
+        f.close()
+    return None
 
 def getWeather():
     xinzhi_apiKey = "junmywaobnqxw8zl"
@@ -15,6 +23,10 @@ def getWeather():
     # 此处只取今天和明天2天的预报
     r = requests.get(url)
     w = r.json()["results"][0]["daily"]
+
+    todayJsonString = json.dumps(w[0])
+    saveToFile(todayJsonString)
+
     today = "今天是%s，白天%s，晚上%s，最高气温%s，最低气温%s" % (w[0]["date"], w[0]["text_day"], w[0]["text_night"], w[0]["high"], w[0]["low"])
     tomorrow = "明天是%s，白天%s，晚上%s，最高气温%s，最低气温%s" % (w[1]["date"], w[1]["text_day"], w[1]["text_night"], w[1]["high"], w[1]["low"])
     message = tomorrow
@@ -32,4 +44,4 @@ def sendMessage(message):
 
 if __name__ == "__main__":
     weather = getWeather()
-    sendMessage(weather)
+    # sendMessage(weather)
