2513bbf6d8922257d734cd9e5a9bbe3fdc3877dc
sivanWu <>
2019-01-04 16:17:54 +0800

    first commit

---
+++ weather.py
@@ -0,0 +1,35 @@
+# -*- coding:UTF-8 -*-
+
+import requests
+from twilio.rest import Client
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def getWeather():
+    xinzhi_apiKey = "junmywaobnqxw8zl"
+    location = "shenzhen"
+    url = "https://api.thinkpage.cn/v3/weather/daily.json?key=%s&location=%s&language=zh-Hans&unit=c&start=0&days=2" % (xinzhi_apiKey, location)
+    # 获取天气预报信息
+    # 此处只取今天和明天2天的预报
+    r = requests.get(url)
+    w = r.json()["results"][0]["daily"]
+    today = "今天是%s，白天%s，晚上%s，最高气温%s，最低气温%s" % (w[0]["date"], w[0]["text_day"], w[0]["text_night"], w[0]["high"], w[0]["low"])
+    tomorrow = "明天是%s，白天%s，晚上%s，最高气温%s，最低气温%s" % (w[1]["date"], w[1]["text_day"], w[1]["text_night"], w[1]["high"], w[1]["low"])
+    message = tomorrow
+    return message
+
+def sendMessage(message):
+    # 设置twilio账户信息
+    twilio_account_sid = "AC5bf3f9be7a84e1dc1555ee11b06d38ec"
+    twilio_auth_token = "b3f2f395c3d6fc51c842cd46a4d73b3f"
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    # 注意to和from_两个参数所代表的手机号，都需要带有国家代码。如中国大陆手机号即+86开头再加上自己的手机号。from_中的号码直接复制twilio提供的号码即可
+    client.messages.create(to="+8615626474791", from_="+18508208255", body=message)
+    return None
+
+if __name__ == "__main__":
+    weather = getWeather()
+    sendMessage(weather)
